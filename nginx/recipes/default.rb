c013148405f1c007fed7fb231918914171d44f9b
chinmaya patnaik <chinmaya patnaik>
2016-10-24 16:46:35 +0530

    Adding nginx and epel installation

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -1,5 +1,8 @@
-#
-# Cookbook Name:: nginx
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+package 'epel-release' do
+    action :install
+end
+
+
+package 'nginx' do
+    action :install
+end
